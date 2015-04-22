1146306485aa32306224974195a85e11d7a4e97d
Jonathan Lepolt <lepolt@batterii.com>
2015-04-22 13:30:31 -0400

    [https://trello.com/c/4xidhJKX/1232-8-asset-tagging] Fix bug w/ where to create new tags when none exist

--- jquery.tag-editor.js
+++ jquery.tag-editor.js
@@ -113,8 +113,8 @@
                 if (closest_tag && closest_tag.length) {
                     loc = 'before';
                 } else if (o.appendNewTags) {
-                    loc = 'after';
                     closest_tag = $('.tag-editor-tag', ed).last();
+                    loc = closest_tag.length ? 'after' : undefined;
                 } else {
                     // calculate tag closest to click position
                     $('.tag-editor-tag', ed).each(function(){
